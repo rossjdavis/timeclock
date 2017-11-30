18ae5ddb08b35c4e5490cc1b5fe4527c4fed13cf
Ross Davis <rossjdavis@gmail.com>
2017-11-30 11:21:05 -0500

    cant register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :trackable, :validatable,
+  devise :database_authenticatable, :trackable, :validatable,
          :authentication_keys => [:username]
 
   has_many :logs, dependent: :destroy
